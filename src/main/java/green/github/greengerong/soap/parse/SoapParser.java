157c272dd4d234a81275407281d9f176ed11de39
greengerongg <grzx2210@163.com>
2014-02-25 08:05:08 +0800

    Initial commit

--- src/main/java/green/github/greengerong/soap/parse/SoapParser.java
+++ src/main/java/green/github/greengerong/soap/parse/SoapParser.java
@@ -1,26 +1,68 @@
 package green.github.greengerong.soap.parse;
 
 
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.MimeHeaders;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.StringReader;
+import java.io.StringWriter;
 
 public class SoapParser {
 
-    public static final String SOAP_ENV_BODY = "SOAP-ENV:Body";
-
     public <T> T getResult(String xml, Class<T> type) {
-        return getInstance(getSoapBody(xml), type);
+        final String soapBody = getSoapBody(xml);
+        return getInstance(soapBody, type);
     }
 
     private String getSoapBody(String xml) {
-        final String startTag = String.format("<%s>", SOAP_ENV_BODY);
-        final int start = xml.indexOf(startTag) + startTag.length();
-        final int end = xml.indexOf(String.format("</%s>", SOAP_ENV_BODY));
-        return xml.substring(start + 1, end);
+        try {
+            SOAPMessage message = getSoapMessage(xml);
+            Node firstElement = getFirstElement(message);
+            return toXml(firstElement);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    private String toXml(Node firstElement) throws TransformerException {
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        final StringWriter sw = new StringWriter();
+        transformer.transform(new DOMSource(firstElement), new StreamResult(sw));
+        return sw.toString();
+    }
+
+    private SOAPMessage getSoapMessage(String xml) throws SOAPException, IOException {
+        MessageFactory factory = MessageFactory.newInstance();
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(xml.getBytes("UTF-8"));
+        return factory.createMessage(new MimeHeaders(), byteArrayInputStream);
+    }
+
+    private Node getFirstElement(SOAPMessage message) throws SOAPException {
+        final NodeList childNodes = message.getSOAPBody().getChildNodes();
+        Node firstElement = null;
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            if (childNodes.item(i) instanceof Element) {
+                firstElement = childNodes.item(i);
+                break;
+            }
+        }
+        return firstElement;
+    }
 
     private <T> T getInstance(String body, Class<T> type) {
         T instance = null;
--- src/test/java/green/github/greengerong/soap/parse/SoapParserTest.java
+++ src/test/java/green/github/greengerong/soap/parse/SoapParserTest.java
@@ -1,15 +1,32 @@
 package green.github.greengerong.soap.parse;
 
 
+import com.google.common.collect.Lists;
 import green.github.greengerong.soap.parse.model.GetOrdersHistoryResponse;
 import green.github.greengerong.soap.parse.model.OrderStatus;
 import green.github.greengerong.soap.parse.model.Statue;
 import org.apache.commons.io.IOUtil;
 import org.junit.Before;
 import org.junit.Test;
+import org.w3c.dom.*;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.soap.*;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
@@ -27,8 +44,7 @@ public class SoapParserTest {
     @Test
     public void shouldGetOrdersHistoryRes() throws IOException {
         //given
-        final InputStream stream = SoapParserTest.class.getResourceAsStream("/GetOrdersHistoryResponse.xml");
-        String xml = IOUtil.toString(stream);
+        String xml = getResponse();
         //when
         GetOrdersHistoryResponse result = soapParser.getResult(xml, GetOrdersHistoryResponse.class);
 
@@ -40,5 +56,10 @@ public class SoapParserTest {
         assertThat(first.getActionCode(), is("actionCode"));
         assertThat(first.getCreateBy().toString(), is("2010-12-1"));
         assertThat(first.getStatue(), is(Statue.None));
+    }
+
+    private String getResponse() throws IOException {
+        final InputStream stream = SoapParserTest.class.getResourceAsStream("/GetOrdersHistoryResponse.xml");
+        return IOUtil.toString(stream);
     }
 }
